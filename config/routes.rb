9d3df2e75a6720e40101c8bee1cf1b2b5f695f6d
Alexis Gonzalez <1991.alexis@gmail.com>
2021-11-03 12:24:58 -0400

    Locations creation ready

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tours#home'
   resources :tours do
-    resources :locations, only: %i[new create destroy]
     resources :bookings, only: %i[new create destroy]
   end
+  resources :locations, only: %i[new create destroy]
 end
